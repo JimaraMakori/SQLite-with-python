c6f8c34f3d810378f2904144787d0a1c7f625b29
JimaraMakori <Jimara.moss@gmail.com>
2026-01-26 18:45:48 +0300

    sqlite in python

---
+++ main.py
@@ -0,0 +1,12 @@
+import sqlite3
+
+database = 'coding.sqlite'
+
+conn = sqlite3.connect(database)
+
+print("Opened database successfully")
+
+import pandas as pd
+tables = pd.read_sql ("SELECT * FROM sqlite_master WHERE type='table';", conn)
+
+print (tables)
