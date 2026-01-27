ef2b47842b2f4b89ab40542eb5cd32ea227950a9
JimaraMakori <Jimara.moss@gmail.com>
2026-01-27 18:54:39 +0300

    where min Where max Where Like clauses SELECT

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-database = 'coding.sqlite'
+database = 'database.sqlite'
 
 conn = sqlite3.connect(database)
 
@@ -10,3 +10,28 @@ import pandas as pd
 tables = pd.read_sql ("SELECT * FROM sqlite_master WHERE type='table';", conn)
 
 print (tables)
+teams = pd.read_sql ("""
+SELECT *
+FROM Team;
+""",conn)
+print (teams)
+
+Matches = pd.read_sql ("""
+SELECT Team_1,Team_2,Win_Margin 
+FROM Match
+WHERE Match_Winner == 7;
+""",conn)
+print (Matches)
+
+new_teams = pd.read_sql ("""
+SELECT *
+FROM Team
+WHERE Team_Name LIKE "P%";
+""",conn)
+print (new_teams)
+
+min_max = pd.read_sql ("""
+SELECT min(Win_Margin),max(Win_Margin)
+FROM Match;
+""",conn)
+print (min_max)
